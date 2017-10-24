97ae4ab693d00442fcfa3c5d7752c3c12a8baae3
duanevt <duanevantwest@gmail.com>
2017-10-24 17:03:11 +1000

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+	setTimeout(function() {
+		$('.main-container').addClass('active');
+		setTimeout(function(){
+			$('.profile-picture').addClass('unison');
+		}, 2500);
+	}, 100);
+
+});
+	
